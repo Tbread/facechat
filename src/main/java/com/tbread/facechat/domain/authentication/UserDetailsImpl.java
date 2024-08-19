5401412ba1b8c128d7b4f6ac018bc1fce06327ca
Tbread <itaebread@gmail.com>
2024-08-19 20:28:50 +0900

    Feat: UserDetails 관련 구현체 완성

--- src/main/java/com/tbread/facechat/domain/authentication/UserDetailsImpl.java
+++ src/main/java/com/tbread/facechat/domain/authentication/UserDetailsImpl.java
@@ -1,17 +1,19 @@
 package com.tbread.facechat.domain.authentication;
 
 import com.tbread.facechat.domain.user.entity.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 public class UserDetailsImpl implements UserDetails {
-    private User user;
+    private final User user;
 
-    public User getUser() {
-        return user;
+    public UserDetailsImpl(final User user) {
+        this.user = user;
     }
 
     @Override
--- src/main/java/com/tbread/facechat/domain/authentication/UserDetailsServiceImpl.java
+++ src/main/java/com/tbread/facechat/domain/authentication/UserDetailsServiceImpl.java
@@ -1,17 +1,27 @@
 package com.tbread.facechat.domain.authentication;
 
+import com.tbread.facechat.domain.user.UserRepository;
+import com.tbread.facechat.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-    
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()){
+            throw new UsernameNotFoundException(username);
+        }
+        return new UserDetailsImpl(userOptional.get());
     }
 }
