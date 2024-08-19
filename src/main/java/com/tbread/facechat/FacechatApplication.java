de61545ccaa0a4a4128690ed23f0aa5f667cd3ff
Tbread <itaebread@gmail.com>
2024-08-19 17:42:31 +0900

    initial commit

---
+++ src/main/java/com/tbread/facechat/FacechatApplication.java
@@ -0,0 +1,13 @@
+package com.tbread.facechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacechatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tbread/facechat/FacechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbread.facechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
