c1827974005978ecae0de9c7acfbdc0990e7747b
Tbread <itaebread@gmail.com>
2024-08-19 19:40:22 +0900

    Feat: User 엔티티 내 메소드 및 빌더패턴 추가

--- src/main/java/com/tbread/facechat/domain/user/entity/User.java
+++ src/main/java/com/tbread/facechat/domain/user/entity/User.java
@@ -2,10 +2,15 @@ package com.tbread.facechat.domain.user.entity;
 
 import com.tbread.facechat.domain.common.Timestamped;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class User extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +25,17 @@ public class User extends Timestamped {
     @Column
     private LocalDateTime lastLoginAt;
 
+    @Builder
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void updateLastLoginAt(){
+        this.lastLoginAt = LocalDateTime.now();
+    }
+
+    public void updatePassword(String password){
+        this.password = password;
+    }
 }
