392c5c8be1caee7bd4e2dea94bdbabe1a70c765b
Tbread <itaebread@gmail.com>
2024-08-27 20:16:57 +0900

    Feat: 로그인 서비스 로직 작성

--- src/main/java/com/tbread/facechat/domain/user/UserService.java
+++ src/main/java/com/tbread/facechat/domain/user/UserService.java
@@ -1,9 +1,17 @@
 package com.tbread.facechat.domain.user;
 
 import com.tbread.facechat.domain.authentication.jwt.JwtProcessor;
+import com.tbread.facechat.domain.common.Result;
+import com.tbread.facechat.domain.user.dto.request.UsernameAndPasswordRequestDto;
+import com.tbread.facechat.domain.user.dto.response.LoginResponseDto;
+import com.tbread.facechat.domain.user.entity.User;
+import com.tbread.facechat.util.ExpiringHashMap;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +22,20 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final JwtProcessor jwtProcessor;
+
+    private final static ExpiringHashMap<String,Boolean> INVALIDATED_REFRESH_TOKEN = new ExpiringHashMap<>();
+
+    public Result<LoginResponseDto> login(UsernameAndPasswordRequestDto req){
+        Optional<User> userOptional = userRepository.findByUsername(req.username());
+        if (userOptional.isEmpty()){
+            return new Result<>("잘못된 아이디 또는 패스워드입니다.",HttpStatus.BAD_REQUEST,false);
+        }
+        if (!passwordEncoder.matches(req.password(),userOptional.get().getPassword())){
+            return new Result<>("잘못된 아이디 또는 패스워드입니다.",HttpStatus.BAD_REQUEST,false);
+        }
+        String refreshToken = jwtProcessor.createToken(userOptional.get(), JwtProcessor.JwtType.REFRESH);
+        String accessToken = jwtProcessor.createToken(userOptional.get(), JwtProcessor.JwtType.ACCESS);
+        
+        return new Result<>(HttpStatus.OK,new LoginResponseDto(refreshToken,accessToken),true);
+    }
 }
