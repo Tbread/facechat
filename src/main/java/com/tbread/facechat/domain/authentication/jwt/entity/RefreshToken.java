9f69a4f497aa8607eb0d4c8c4b362e0450639260
Tbread <itaebread@gmail.com>
2024-08-27 19:57:06 +0900

    Feat: 빌더 패턴 추가

--- src/main/java/com/tbread/facechat/domain/authentication/jwt/entity/RefreshToken.java
+++ src/main/java/com/tbread/facechat/domain/authentication/jwt/entity/RefreshToken.java
@@ -2,6 +2,7 @@ package com.tbread.facechat.domain.authentication.jwt.entity;
 
 import com.tbread.facechat.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,4 +28,10 @@ public class RefreshToken {
     private LocalDateTime expiredAt;
     //추후 mysql에서 해당 컬럼 기반으로 제거 스케쥴러 설정 혹은 자체 스케쥴러 클래스 작성 필요
 
+    @Builder
+    public RefreshToken(User user,String token,LocalDateTime expiredAt){
+        this.user = user;
+        this.token = token;
+        this.expiredAt = expiredAt;
+    }
 }
