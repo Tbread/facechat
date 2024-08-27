514747ee9bee297190af8c4f6fee7a8b0408c728
Tbread <itaebread@gmail.com>
2024-08-27 20:50:47 +0900

    Feat: 로그아웃 로직 작성

--- src/main/java/com/tbread/facechat/domain/user/UserService.java
+++ src/main/java/com/tbread/facechat/domain/user/UserService.java
@@ -1,18 +1,21 @@
 package com.tbread.facechat.domain.user;
 
 import com.tbread.facechat.domain.authentication.jwt.JwtProcessor;
+import com.tbread.facechat.domain.authentication.userdetails.UserDetailsImpl;
 import com.tbread.facechat.domain.common.Result;
 import com.tbread.facechat.domain.user.dto.request.UsernameAndPasswordRequestDto;
 import com.tbread.facechat.domain.user.dto.response.LoginResponseDto;
 import com.tbread.facechat.domain.user.entity.User;
 import com.tbread.facechat.util.ExpiringHashMap;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @Service
@@ -25,15 +28,15 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtProcessor jwtProcessor;
 
-    private final static ExpiringHashMap<String,Boolean> INVALIDATED_REFRESH_TOKEN = new ExpiringHashMap<>();
+    private final static ExpiringHashMap<String, Boolean> INVALIDATED_REFRESH_TOKEN = new ExpiringHashMap<>();
 
-    public Result<LoginResponseDto> login(HttpServletResponse res,UsernameAndPasswordRequestDto req){
+    public Result<LoginResponseDto> login(HttpServletResponse httpRes, UsernameAndPasswordRequestDto req) {
         Optional<User> userOptional = userRepository.findByUsername(req.username());
-        if (userOptional.isEmpty()){
-            return new Result<>("잘못된 아이디 또는 패스워드입니다.",HttpStatus.BAD_REQUEST,false);
+        if (userOptional.isEmpty()) {
+            return new Result<>("잘못된 아이디 또는 패스워드입니다.", HttpStatus.BAD_REQUEST, false);
         }
-        if (!passwordEncoder.matches(req.password(),userOptional.get().getPassword())){
-            return new Result<>("잘못된 아이디 또는 패스워드입니다.",HttpStatus.BAD_REQUEST,false);
+        if (!passwordEncoder.matches(req.password(), userOptional.get().getPassword())) {
+            return new Result<>("잘못된 아이디 또는 패스워드입니다.", HttpStatus.BAD_REQUEST, false);
         }
         String refreshToken = jwtProcessor.createToken(userOptional.get(), JwtProcessor.JwtType.REFRESH);
         String accessToken = jwtProcessor.createToken(userOptional.get(), JwtProcessor.JwtType.ACCESS);
@@ -41,14 +44,36 @@ public class UserService {
         Cookie accessCookie = new Cookie("Access-Token", accessToken);
         accessCookie.setPath("/");
         accessCookie.setHttpOnly(true);
-        res.addCookie(accessCookie);
+        httpRes.addCookie(accessCookie);
 
         Cookie refreshCookie = new Cookie("Refresh-Token", refreshToken);
         refreshCookie.setPath("/");
         refreshCookie.setHttpOnly(true);
-        res.addCookie(refreshCookie);
+        httpRes.addCookie(refreshCookie);
 
         //백엔드 - 프론트서버의 비 분리 사용으로 편의성을 위해 쿠키로 설정, 추후 필터체인으로 쿠키 관리 필요
-        return new Result<>(HttpStatus.OK,new LoginResponseDto(refreshToken,accessToken),true);
+        return new Result<>(HttpStatus.OK, new LoginResponseDto(refreshToken, accessToken), true);
+    }
+
+    public Result logout(HttpServletRequest httpReq, HttpServletResponse httpRes) {
+        Cookie accessCookie = new Cookie("Access-Token", null);
+        accessCookie.setMaxAge(0);
+        accessCookie.setPath("/");
+        accessCookie.setHttpOnly(true);
+        httpRes.addCookie(accessCookie);
+        Cookie refreshCookie = new Cookie("Refresh-Token", null);
+        refreshCookie.setMaxAge(0);
+        refreshCookie.setPath("/");
+        refreshCookie.setHttpOnly(true);
+        httpRes.addCookie(refreshCookie);
+
+        if (httpReq.getCookies() != null) {
+            Optional<Cookie> cookieOptional = Arrays.stream(httpReq.getCookies()).filter(c -> c.getName().equals("Refresh-Token")).findFirst();
+            if (cookieOptional.isPresent()){
+                String refreshToken = cookieOptional.get().getValue();
+                INVALIDATED_REFRESH_TOKEN.put(refreshToken,true,jwtProcessor.getExpiration(refreshToken));
+            }
+        }
+        return new Result<>(HttpStatus.OK,true);
     }
 }
