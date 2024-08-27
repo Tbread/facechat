c941ca7825cb76a3b60964b56a93b94efbd1f2f8
Tbread <itaebread@gmail.com>
2024-08-27 20:23:12 +0900

    Feat: 로그인 로직 추가
    프론트-백엔드서버의 비 분리로 편의성을 위해 쿠키 설정 추가

--- src/main/java/com/tbread/facechat/domain/user/UserService.java
+++ src/main/java/com/tbread/facechat/domain/user/UserService.java
@@ -6,6 +6,8 @@ import com.tbread.facechat.domain.user.dto.request.UsernameAndPasswordRequestDto
 import com.tbread.facechat.domain.user.dto.response.LoginResponseDto;
 import com.tbread.facechat.domain.user.entity.User;
 import com.tbread.facechat.util.ExpiringHashMap;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,7 +27,7 @@ public class UserService {
 
     private final static ExpiringHashMap<String,Boolean> INVALIDATED_REFRESH_TOKEN = new ExpiringHashMap<>();
 
-    public Result<LoginResponseDto> login(UsernameAndPasswordRequestDto req){
+    public Result<LoginResponseDto> login(HttpServletResponse res,UsernameAndPasswordRequestDto req){
         Optional<User> userOptional = userRepository.findByUsername(req.username());
         if (userOptional.isEmpty()){
             return new Result<>("잘못된 아이디 또는 패스워드입니다.",HttpStatus.BAD_REQUEST,false);
@@ -35,7 +37,18 @@ public class UserService {
         }
         String refreshToken = jwtProcessor.createToken(userOptional.get(), JwtProcessor.JwtType.REFRESH);
         String accessToken = jwtProcessor.createToken(userOptional.get(), JwtProcessor.JwtType.ACCESS);
-        
+
+        Cookie accessCookie = new Cookie("Access-Token", accessToken);
+        accessCookie.setPath("/");
+        accessCookie.setHttpOnly(true);
+        res.addCookie(accessCookie);
+
+        Cookie refreshCookie = new Cookie("Refresh-Token", refreshToken);
+        refreshCookie.setPath("/");
+        refreshCookie.setHttpOnly(true);
+        res.addCookie(refreshCookie);
+
+        //백엔드 - 프론트서버의 비 분리 사용으로 편의성을 위해 쿠키로 설정, 추후 필터체인으로 쿠키 관리 필요
         return new Result<>(HttpStatus.OK,new LoginResponseDto(refreshToken,accessToken),true);
     }
 }
