191ba16fd4d824646fa0049cac4aa9ed61a24a4b
Tbread <itaebread@gmail.com>
2024-08-29 01:37:53 +0900

    Feat: JWT 체크를 위한 필터체인 클래스 작성

---
+++ src/main/java/com/tbread/facechat/domain/authentication/jwt/JwtFilterChain.java
@@ -0,0 +1,47 @@
+package com.tbread.facechat.domain.authentication.jwt;
+
+import com.tbread.facechat.domain.common.TokenPackage;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilterChain extends GenericFilterBean {
+    private final JwtProcessor jwtProcessor;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) request;
+        HttpServletResponse httpRes = (HttpServletResponse) response;
+        TokenPackage tokenPackage = jwtProcessor.extractToken(httpReq);
+        if (!tokenPackage.getRefreshToken().isEmpty() && !tokenPackage.getAccessToken().isEmpty()) {
+            if (jwtProcessor.isValidate(tokenPackage.getAccessToken())) {
+                //액세스 토큰 비만료
+                Authentication authentication = jwtProcessor.getAuthentication(tokenPackage.getAccessToken());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                //액세스 토큰 만료
+                if (jwtProcessor.isValidate(tokenPackage.getRefreshToken()) && !jwtProcessor.isInvalidatedToken(tokenPackage)) {
+                    //리프레시 토큰 비만료
+                    String newAccessToken = jwtProcessor.createToken(jwtProcessor.extractUserDetails(tokenPackage.getRefreshToken()).getUser(), JwtProcessor.JwtType.ACCESS);
+                    jwtProcessor.setJwtCookie(httpRes, newAccessToken, JwtProcessor.JwtType.ACCESS);
+                    Authentication authentication = jwtProcessor.getAuthentication(newAccessToken);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    //리프레시 토큰 만료
+                    jwtProcessor.clearJwtCookies(httpRes);
+                }
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
